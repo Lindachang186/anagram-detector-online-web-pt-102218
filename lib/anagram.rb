98a0240bcaea1536a4b92c3dc056e563b64e3312
Linda Chang <lindachang186@gmail.com>
2018-11-16 15:31:55 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    array.each do |item|
+      if item.split(//).sort == word.split(//).sort
+        item
+      end
+    end
+  end
+
+end
